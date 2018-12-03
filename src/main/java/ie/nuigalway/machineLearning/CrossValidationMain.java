071f92258fd0226db723c0d86da51ed0e3e38d10
NitroXcellerate <niallgreaney@gmail.com>
2018-12-03 08:03:17 +0000

    comment for cross val

--- src/main/java/ie/nuigalway/machineLearning/CrossValidationMain.java
+++ src/main/java/ie/nuigalway/machineLearning/CrossValidationMain.java
@@ -2,6 +2,11 @@ package ie.nuigalway.machineLearning;
 
 import java.util.ArrayList;
 
+/**
+ * 10 Fold Cross Validation Main
+ * @author Niall Greaney
+ * @ID 15479942
+ */
 public class CrossValidationMain {
 
 	public static void main(String[] args) {
